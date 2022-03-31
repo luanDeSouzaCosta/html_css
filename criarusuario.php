78c0b0c7fd471ff3f180fe81fb85b86886c646e3
skrigowz <102750160+skrigowz@users.noreply.github.com>
2022-03-31 17:12:35 -0300

    Novidades
    Criado php para criação de usuário

---
+++ criarusuario.php
@@ -0,0 +1,28 @@
+<?php
+
+
+// recebe as variaveis do formulário
+$login = $_POST['nome'];
+$senha = $_POST['senha'];
+$senha2 = $_POST['senha2'];
+$email = $_POST['email']
+
+if ($senha != $senha2) die( 'Senhas diferentes' );
+
+# Substitua abaixo os dados, de acordo com o banco criado
+$user = "368079";
+$password = "LuAm1003";
+'$database = "cadastro";'
+
+# O hostname deve ser sempre localhost
+$hostname = "localhost";
+
+# Conecta com o servidor de banco de dados
+mysql_connect( $hostname, $user, $password ) or die( ' Erro na conexão ' );
+
+# Executa a query desejada
+$query = "INSERT INTO cadastro (email, nome, senha) VALUES (" & $email & "," & $login & "," & senha &")
+";
+$result_query = mysql_query( $query ) or die(' Erro na query:' . $query . ' ' . mysql_error() );
+
+?>
