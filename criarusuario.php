4071da08a5e47c9c6513779e5419447aa275f617
Luan de Souza Costa <85460406+luanDeSouzaCosta@users.noreply.github.com>
2022-03-31 17:25:40 -0300

    ajustes no código
    ajustes

--- criarusuario.php
+++ criarusuario.php
@@ -5,14 +5,15 @@
 $login = $_POST['nome'];
 $senha = $_POST['senha'];
 $senha2 = $_POST['senha2'];
-$email = $_POST['email']
+$email = $_POST['email'];
 
-if ($senha != $senha2) die( 'Senhas diferentes' );
+if ($senha != $senha2){
+    die( 'Senhas diferentes' );
+} 
 
 # Substitua abaixo os dados, de acordo com o banco criado
 $user = "368079";
 $password = "LuAm1003";
-'$database = "cadastro";'
 
 # O hostname deve ser sempre localhost
 $hostname = "localhost";
@@ -21,7 +22,7 @@ $hostname = "localhost";
 mysql_connect( $hostname, $user, $password ) or die( ' Erro na conexão ' );
 
 # Executa a query desejada
-$query = "INSERT INTO cadastro (email, nome, senha) VALUES (" & $email & "," & $login & "," & senha &")
+$query = "INSERT INTO cadastro (email, nome, senha) VALUES (" & $email & "," & $login & "," & $senha &")
 ";
 $result_query = mysql_query( $query ) or die(' Erro na query:' . $query . ' ' . mysql_error() );
 
